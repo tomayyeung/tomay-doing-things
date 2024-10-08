cdb4330e4e87ae62b6420068741456284def7a9e
Thomas Yeung <169122015+tomayyeung@users.noreply.github.com>
2024-10-08 12:27:11 -0700

    hi

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # tomay-doing-things
-This is just my repo for random projects I have in Python.
-
-:D
--- soccer.py
+++ soccer.py
@@ -1,10 +1,11 @@
-import pygame, sys, math
-from pygame.locals import *
+import pygame, sys
+import pygame.locals
+import numpy as np
 
 # ---------------------- define constants
 # pygame
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 FPS = 60
 
 # colors
@@ -12,13 +13,27 @@ GREEN = (0, 170, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
+YELLOW = (190, 190, 0)
 
 # game constants
+FIELD_WIDTH = 600
+FIELD_HEIGHT = 400
+X_GAP = (SCREEN_WIDTH-FIELD_WIDTH)/2
+Y_GAP = (SCREEN_HEIGHT-FIELD_HEIGHT)/2
+GOAL_DEPTH = 45
+GOAL_HEIGHT = 100
+GOAL_TOP = Y_GAP+FIELD_HEIGHT/2-GOAL_HEIGHT/2
+GOAL_BOTTOM = GOAL_TOP+GOAL_HEIGHT
+LEFT_GOAL_BACK = X_GAP-GOAL_DEPTH
+RIGHT_GOAL_BACK = SCREEN_WIDTH-X_GAP+GOAL_DEPTH
+BALL_MASS = 15
+BALL_SIZE = 10
 PLAYER_MASS = 30
 PLAYER_SIZE = 20
 SELECTED_THICKNESS = 5
-MAX_VEL = FPS*2/3 # yo idk what these numbers mean don't ask about units lmao
-RESISTANCE = 0.9 # throw this onto stuff to simulate friction/air resistance/physics??
+MAX_VEL = FPS*0.2 # yo idk what these numbers mean don't ask about units lmao
+AIM_TWEAK = 10 # smaller number = less difference bt big aim and small aim
+RESISTANCE = 0.95 # throw this onto stuff to simulate friction/air resistance/physics??
 
 # ---------------------- define classes
 class PhysicalObject:
@@ -26,7 +41,7 @@ class PhysicalObject:
         self.x = x
         self.y = y
         self.mass = mass
-        self.size = size
+        self.size = size # radius of circle
         self.color = color
 
         self.velX = 0
@@ -44,43 +59,71 @@ class PhysicalObject:
 
     # does NOT check for collision, only handles it
     def handleCollision(self, other):
+        # we handle the moving object differently from the stationary object :skull:
+        if (self.velX, self.velY) == (0, 0):
+            a = self
+            b = other
+        else:
+            a = other
+            b = self
+        # if two moving objects collide then the physics will die but is okay!
+
         # define initial values
-        velAX, velAY = self.velX, self.velY
-        velBX, velBY = other.velX, other.velY
-        collisionAngle = angle(self.x, self.y, other.x, other.y)
+        velAX, velAY = a.velX, a.velY
+        #velBX, velBY = other.velX, other.velY
+        collisionAngle = angle(a.x, a.y, b.x, b.y)
 
         # -------- angle calculation
         # with other object mass = 0, new velocity direction doesn't change
         # with other object mass = inf, new velocity direction reflects off other object
-        selfOriginalAngle = angle(velAX, velAY, 0, 0)
-        selfMaxAngle = math.pi+collisionAngle+(collisionAngle-selfOriginalAngle)
-        angleDiff = selfMaxAngle-selfOriginalAngle
-        selfFinalAngle = selfOriginalAngle + (-1/(other.mass/self.mass) + 1)*angleDiff
+        aOriginalAngle = angle(velAX, velAY, 0, 0)
+        aMaxAngle = np.pi+collisionAngle+(collisionAngle-aOriginalAngle)
+        angleDiff = aMaxAngle-aOriginalAngle
+        aFinalAngle = aOriginalAngle + (-1/(b.mass/a.mass) + 1)*angleDiff
 
         # -------- magnitude calculation
         # the more directly it hits the other object, the less velocity it keeps & the more it transfers
-        selfMagnitude = abs(collisionAngle-selfOriginalAngle)/math.pi * 5 * other.mass/self.mass * RESISTANCE
-        otherMagnitude = abs(selfMagnitude-xyToVector(self.velX,self.velY)[0]) * 1.5 * self.mass/other.mass * RESISTANCE
+        aMagnitude = abs(collisionAngle-aOriginalAngle)/np.pi * 5 * b.mass/a.mass * RESISTANCE
+        bMagnitude = abs(aMagnitude-xyToVector(a.velX,a.velY)[0]) * 1.5 * a.mass/b.mass * RESISTANCE
 
-        self.velX, self.velY = vectorToXY(selfMagnitude, selfFinalAngle)
-        other.velX, other.velY = vectorToXY(otherMagnitude, collisionAngle)
+        a.velX, a.velY = vectorToXY(aMagnitude, aFinalAngle)
+        b.velX, b.velY = vectorToXY(bMagnitude, collisionAngle)
+    
+    # detects and handles collision with the wall
+    def handleWallCollision(self):
+        # field walls
+        # left/right, take into account goal
+        if (self.x-self.size < X_GAP or self.x+self.size > X_GAP+FIELD_WIDTH) and (self.y-self.size < GOAL_TOP or self.y+self.size > GOAL_BOTTOM):
+            self.velX = -self.velX
+        # top/bottom
+        if (self.y-self.size < Y_GAP or self.y+self.size > Y_GAP+FIELD_HEIGHT):
+            self.velY = -self.velY
+        
+        # goal walls
+        # back walls
+        if (self.x-self.size < LEFT_GOAL_BACK or self.x+self.size > RIGHT_GOAL_BACK):
+            self.velX = -self.velX
+        # top/bottom walls, take into account object has to be inside goal
+        if (self.y-self.size < GOAL_TOP or self.y+self.size > GOAL_BOTTOM) and (self.x < X_GAP or self.x > X_GAP + FIELD_WIDTH):
+            self.velY = -self.velY
+
 
 class Player(PhysicalObject):
-    selected = False
+    hovered = False
     def __init__(self, x, y, color):
         super().__init__(x, y, PLAYER_MASS, PLAYER_SIZE, color)
 
     def draw(self, surf):
         super().draw(surf)
-        if self.selected:
+        if self.hovered:
             pygame.draw.circle(surf, WHITE, (self.x, self.y), self.size, SELECTED_THICKNESS)
 
 # ---------------------- define functions
 def distance(x1, y1, x2, y2):
-    return math.sqrt( (x1-x2)**2 + (y1-y2)**2 )
+    return np.sqrt( (x1-x2)**2 + (y1-y2)**2 )
 
 def angle(x1, y1, x2, y2):
-    return math.atan2( (y2-y1), (x2-x1) )
+    return np.atan2( (y2-y1), (x2-x1) )
 
 def xyToVector(velX, velY):
     magnitude = distance(0, 0, velX, velY)
@@ -88,8 +131,8 @@ def xyToVector(velX, velY):
     return magnitude, direction
 
 def vectorToXY(magnitude, direction):
-    x = math.cos(direction)*magnitude
-    y = math.sin(direction)*magnitude
+    x = np.cos(direction)*magnitude
+    y = np.sin(direction)*magnitude
     return x, y
 
 def main():
@@ -99,37 +142,105 @@ def main():
     DISPLAYSURF.fill(GREEN)
     pygame.display.set_caption("Soccer")
     clock = pygame.time.Clock()
+    pygame.font.init()
+    displayFont = pygame.font.SysFont("Comic Sans MS", 80)
 
     # create objects
-    player1 = Player(100, 100, BLUE)
-    player2 = Player(200, 180, RED)
+    ball = PhysicalObject(600, 400, BALL_MASS, BALL_SIZE, WHITE)
+    player1 = Player(200, 200, BLUE)
+    player2 = Player(200, 460, RED)
 
+    objects = [ball, player1, player2]
+    players = objects[1:]
+
+    selected = None
+    startingX, startingY, = 0,0
+    #aimX = 0
+    #aimY = 0
     while 1:
         # handle input
+        # hold click & drag to aim
+        mouseX, mouseY = pygame.mouse.get_pos()
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_a:
-                    player1.velX = 25
-                    player1.velY = 25
+            
+            if event.type == pygame.locals.KEYDOWN:
+                if event.key == pygame.locals.K_a:
+                    player1.velX = 30
+                    player1.velY = 30
+            
+            if event.type == pygame.locals.MOUSEBUTTONDOWN:
+                # on click, check if anything's selected
+                # if not, check the cursor is on any player to mark it as selected
+                if selected is None:
+                    for player in players:
+                        if distance(mouseX, mouseY, player.x, player.y) <= PLAYER_SIZE:
+                            # distance from any player is within the player size = mouse is on the circle
+                            startingX, startingY = mouseX, mouseY
+                            player.hovered = True # mark to draw the circle around it
+                            selected = player
+                            break
+            
+            if event.type == pygame.locals.MOUSEBUTTONUP:
+                # on unclick, check if anything's selected
+                # if so, check if the cursor's outside the player
+                if selected: # THANKS ALEX
+                    # Calculate the drag distance (difference between release and starting point)
+                    deltaX = mouseX - startingX
+                    deltaY = mouseY - startingY
+                
+                    # Calculate the velocity to be applied based on the drag distance
+                    aimX = deltaX / AIM_TWEAK
+                    aimY = deltaY / AIM_TWEAK
 
-        mouseX, mouseY = pygame.mouse.get_pos()
+                    # Limit the velocity to MAX_VEL
+                    magnitude = min(np.sqrt(aimX**2 + aimY**2), MAX_VEL)
+                    direction = np.atan2(aimY, aimX)
 
-        player1.selected = distance(mouseX, mouseY, player1.x, player1.y) <= PLAYER_SIZE
-        player2.selected = distance(mouseX, mouseY, player2.x, player2.y) <= PLAYER_SIZE
+                    # Apply the velocity
+                    selected.velX = -magnitude * np.cos(direction)
+                    selected.velY = -magnitude * np.sin(direction)
 
-        if distance(player1.x, player1.y, player2.x, player2.y) <= PLAYER_SIZE*2:
-            player1.handleCollision(player2)
+                    player.hovered = False
+                    selected = None
+                # if not, unselect the thing
+                else:
+                    player.hovered = False
+                    selected = None
 
-        player1.updatePos()
-        player2.updatePos()
+        # check for wall collision
+        for obj in objects:
+            # update twice to handle goal corners!
+            obj.handleWallCollision()
+            obj.updatePos()
+            obj.handleWallCollision()
+            obj.updatePos()
+
+        pairs = [(a, b) for i, a in enumerate(objects) for b in objects[i+1:]]
+        for obj1, obj2 in pairs:
+            if distance(obj1.x, obj1.y, obj2.x, obj2.y) <= obj1.size+obj2.size:
+                obj1.handleCollision(obj2)
+        
 
         # display
         DISPLAYSURF.fill(GREEN)
-        player1.draw(DISPLAYSURF)
-        player2.draw(DISPLAYSURF)
+        pygame.draw.rect(DISPLAYSURF, WHITE, pygame.Rect(X_GAP, Y_GAP, FIELD_WIDTH, FIELD_HEIGHT), 1) # field lines
+        pygame.draw.rect(DISPLAYSURF, WHITE, pygame.Rect(LEFT_GOAL_BACK, GOAL_TOP, GOAL_DEPTH, GOAL_HEIGHT), 1) # left goal
+        pygame.draw.rect(DISPLAYSURF, WHITE, pygame.Rect(X_GAP+FIELD_WIDTH, GOAL_TOP, GOAL_DEPTH, GOAL_HEIGHT), 1) # right goal
+        pygame.draw.line(DISPLAYSURF, YELLOW, (X_GAP, GOAL_TOP), (X_GAP, GOAL_BOTTOM), 4) # left goal line
+        pygame.draw.line(DISPLAYSURF, YELLOW, (X_GAP+FIELD_WIDTH, GOAL_TOP), (X_GAP+FIELD_WIDTH, GOAL_BOTTOM), 4) # right goal line
+        for obj in objects:
+            obj.draw(DISPLAYSURF)
+        if selected:
+            pygame.draw.line(DISPLAYSURF, WHITE, (selected.x, selected.y), (mouseX, mouseY), SELECTED_THICKNESS)
+        if (ball.x < 0):
+            displayText = displayFont.render("RED SCORE", True, WHITE)
+            DISPLAYSURF.blit(displayText, (100, 100))
+        if (ball.x > X_GAP+FIELD_WIDTH):
+            displayText = displayFont.render("BLUE SCORE", True, WHITE)
+            DISPLAYSURF.blit(displayText, (100, 100))
 
         # update window
         pygame.display.update()
